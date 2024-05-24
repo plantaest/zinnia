f24545ff39883b9cb070e3adad5be44222368734
Plantaest <plantaest@gmail.com>
2024-05-24 16:14:47 +0700

    release: 0.1.0-alpha.4

--- apps/zinnia-core/src/utils/migration/versionMap.ts
+++ apps/zinnia-core/src/utils/migration/versionMap.ts
@@ -3,4 +3,5 @@ export const versionMap: Record<string, string> = {
   '0.1.0-alpha.1': '0.1.0-alpha.1',
   '0.1.0-alpha.2': '0.1.0-alpha.1',
   '0.1.0-alpha.3': '0.1.0-alpha.1',
+  '0.1.0-alpha.4': '0.1.0-alpha.1',
 };
