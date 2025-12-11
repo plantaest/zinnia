5838980db81022fc8e2b6b1dc4528614d9e98fbc
Plantaest <plantaest@gmail.com>
2025-12-11 14:26:35 +0700

    release: 0.1.0-alpha.9

--- apps/zinnia-core/src/utils/migration/versionMap.ts
+++ apps/zinnia-core/src/utils/migration/versionMap.ts
@@ -8,4 +8,6 @@ export const versionMap: Record<string, string> = {
   '0.1.0-alpha.6': '0.1.0-alpha.1',
   '0.1.0-alpha.7': '0.1.0-alpha.1',
   '0.1.0-alpha.8': '0.1.0-alpha.1',
+  // Schema version should be 0.1.0-alpha.2 since UserConfig has changed significantly, but I don't have time.
+  '0.1.0-alpha.9': '0.1.0-alpha.1',
 };
